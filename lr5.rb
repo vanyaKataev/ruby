6668c734078ea8e58ab3309269e6095bac500f7c
vanyaKataev <84904843+vanyaKataev@users.noreply.github.com>
2021-06-16 22:11:14 +0300

    Add files via upload

---
+++ lr5.rb
@@ -0,0 +1,75 @@
+require 'socket'
+
+server = TCPServer.new(3000)
+
+PATH = 'balance.txt'.freeze
+STARTING_BALANCE = 100
+
+class CashMachine
+  def initialize
+    if File.exists?(PATH.to_s) 
+      balance_value = File.open(PATH.to_s, 'r')
+      @balance = balance_value.read.chomp.to_f
+    else
+      balance_value = File.open(PATH.to_s, 'w')
+      balance_value.puts(STARTING_BALANCE)
+      balance_value.close
+      balance_value = File.open(PATH.to_s, 'r')
+      @balance = balance_value.read.chomp.to_f
+    end
+  end
+
+  def withdraw(withdraw_value)
+    return 'Error! Negative number entered.' if withdraw_value.negative?
+    return 'Error! Not enough funds.' if @balance < withdraw_value
+    @balance -= withdraw_value
+    balance_value = File.open(PATH.to_s, 'w')
+    balance_value.puts(@balance)
+    balance_value.close
+    "New balance: #{@balance}"
+  end
+
+  def deposit(deposit_value)
+    return 'Error! Negative number entered.' if deposit_value.negative?
+    @balance += deposit_value
+    balance_value = File.open(PATH.to_s, 'w')
+    balance_value.puts(@balance)
+    balance_value.close
+    "New balance: #{@balance}"
+  end
+
+  def balance
+    "Balance: #{@balance}"
+  end
+
+end
+
+while (connection = server.accept)
+
+  cash_machine = CashMachine.new
+
+  request = connection.gets
+  m, full_path = request.split(' ')
+  path = full_path.split('/')[1]
+  if full_path.split('/')[1].include?('?')
+    method = path.split('?')[0]
+    value = path.split('?')[1].split('=')[1].to_i
+  end
+
+  connection.print "HTTP/1.1 200\r\n"
+  connection.print "Content-Type: text/html\r\n"
+  connection.print "\r\n"
+  unless value.nil?
+    connection.print case method
+                     when 'deposit'
+                       cash_machine.deposit(value)
+                     when 'withdraw'
+                       cash_machine.withdraw(value)
+                     else
+                       'error'
+                     end
+  end
+  method = nil
+  value = nil
+  connection.print cash_machine.balance if path =='balance'
+end
